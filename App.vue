1c7082b2617f721a72ee92509b8599149d478628
hanocha <saiki@feedforce.jp>
2018-02-17 18:12:54 +0900

    tmp(not working)

---
+++ App.vue
@@ -0,0 +1,15 @@
+<template>
+  <App>
+    <h1 :class="$style.title">Hello Vue with template!!</h1>
+  </App>
+</template>
+
+<style module lang="scss">
+  .title {
+    font-size: 10px;
+  }
+</style>
+
+<script>
+  export default {};
+</script>
--- index.js
+++ index.js
@@ -1,8 +1,16 @@
-import Vue from 'vue'
+import Vue from 'vue';
+import lang from 'element-ui/lib/locale/lang/ja';
+import locale from 'element-ui/lib/locale';
+import App from './App';
+
+locale.use(lang);
 
 new Vue({
   el: '#app',
+  components: { App },
   render() {
-    return <h1>Hello Vue.</h1>
-  }
-})
+    <div id="app">
+      <App />
+    </div>
+  },
+});
